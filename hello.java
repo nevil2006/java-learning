d276b538bbb4441759ecba62b4aaff7dcbbf8f79
NEVIL <nevilj22@gmail.com>
2025-10-04 08:23:53 +0530

    hello.java

--- hello.java
+++ hello.java
@@ -3,9 +3,11 @@ import java.util.Scanner;
 
 class hello {
     public static void main(String args[]) {
-        Scanner john = new Scanner(System.in);  // take input from keyboard
-        int a = john.nextInt();                 // read an integer
-        System.out.println("You entered: " + a);
-        john.close();                           // always close Scanner
+        Scanner john = new Scanner(System.in);  // create a Scanner object
+        int a = john.nextInt();                 // read first integer
+        int b = john.nextInt();  
+        int sum =a + b;
+        System.out.print("sum=" + sum);               // print sum
+        john.close();                           // close Scanner
     }
 }
