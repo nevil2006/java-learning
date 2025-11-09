725fe8ec8732b2f36f5affaf3f74a1fede1400fc
NEVIL <147979766+nevil2006@users.noreply.github.com>
2025-11-09 20:18:56 +0530

    local updates

---
+++ data.java
@@ -0,0 +1,16 @@
+public class data {
+    public static void main(String[] args) {
+            int age = 21000000;
+            System.out.println(age); 
+
+            float height = 5.9f;
+            System.out.println(height);
+
+            boolean isStudent = false;
+            System.out.println(isStudent);
+
+            char grade = 'A';
+            System.out.println(grade);
+    }
+    
+}
