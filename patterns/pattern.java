633cf2b52e1d1e35de47032e66b0de22cb874f79
NEVIL <147979766+nevil2006@users.noreply.github.com>
2025-12-16 19:07:15 +0530

    first commit

--- patterns/pattern.java
+++ patterns/pattern.java
@@ -1,16 +1,14 @@
 public class pattern {
 		
-	public static void main(String[] args) {
-		System.out.println();
-		
-		int n= 5;
-		
-		for(int row=n;row>=1;row--,System.out.println()) 
-		{
-			for (int col =row ; col<=n ; col++) {
-				System.out.print(col);
-			}
-				
-		}
-	}
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int row = n; row >= 1; row--) {
+            for (int col = n; col >= n - row + 1; col--) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
 }
