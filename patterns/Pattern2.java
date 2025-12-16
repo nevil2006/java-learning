76ebfd74bb30f51d32c4baaedc0afddc1756b0e0
NEVIL <147979766+nevil2006@users.noreply.github.com>
2025-12-16 18:42:29 +0530

    first commit

---
+++ patterns/Pattern2.java
@@ -0,0 +1,13 @@
+public class Pattern2 {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int row = 1; row <= n; row++) {
+            for (int col = row; col >=1; col--) {
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+}
