965a4b95393d28654ce5982fb9bc4342393dcb09
NEVIL <147979766+nevil2006@users.noreply.github.com>
2025-12-18 06:20:12 +0530

    first commit

--- patterns/patternmy.java
+++ patterns/patternmy.java
@@ -1,11 +1,11 @@
 public class patternmy {
     public static void main(String[] args) {
-        int n = 6;
+        int n = 5;
 
-        for (int row = 1; row < n; row++) {
+        for (int row = 0; row <= n; row++) {
 
-            for (int col = 1; col <= row; col++) {
-                System.out.print("*");
+            for (int col = 1; col <= row+1; col++) {
+                System.out.print(col);
             }
 
             System.out.println(); 
