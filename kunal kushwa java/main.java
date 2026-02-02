49497f5937ec5b3a4a2ac71ba31c02911419b121
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-02-03 03:51:28 +0530

    first commit

--- kunal kushwa java/main.java
+++ kunal kushwa java/main.java
@@ -1,7 +1,15 @@
-public class main {
+import java.util.Scanner;
 
+public class main {
     public static void main(String[] args) {
-        System.out.println("gunal");
-        System.out.println("Hello, Kunal Kushwa!");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = sc.nextInt();
+        
+        System.out.println("My age is: " + age);
+
+        sc.close();
     }
 }
