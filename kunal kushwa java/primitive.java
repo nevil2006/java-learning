9c56ab6008d849e26a3e203b8258b9d22b6c5864
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-02-03 04:03:50 +0530

    first commit

--- kunal kushwa java/primitive.java
+++ kunal kushwa java/primitive.java
@@ -1,5 +1,10 @@
 
 public class primitive {
-    
+    public static void main(String[] args) {
+        int rollno = 64;
+
+        String name="kunal kushwa";
+        System.err.println(name); ;
+    }
     
 }
