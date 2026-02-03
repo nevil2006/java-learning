a01961d3f7f87df82e8e7fc75a37d112609f15f4
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-02-04 05:05:03 +0530

    first commit

---
+++ kunal kushwa java/practice questions/five.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class five{
+    public static void main(String[]args){
+            Scanner Sc=new Scanner(System.in);
+            String name = Sc.next();
+            System.out.println("Hello " + name);
+
+        }
+    
+}
