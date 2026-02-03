c7b3728854ecf1369cfbc540c88923d5687e8f8b
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-02-03 09:38:14 +0530

    first commit

--- kunal kushwa java/practice questions/three.java
+++ kunal kushwa java/practice questions/three.java
@@ -1,9 +1,12 @@
 import java.util.Scanner;
+
 public class three {
-    public static void main(String[]args){
-            Scanner sc=new Scanner(System.in);
-            int n=sc.nextInt();
-            System.out.println(n);
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        float n = sc.nextFloat();
+        System.out.printf("%.2f", n);
+
+        sc.close();
     }
-    
 }
