6c5104f5f12c2cab56ddc82f2ca67d184c1f6ae4
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-02-03 05:43:58 +0530

    first commit

---
+++ kunal kushwa java/typecasting.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public  class typecasting{
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+
+        int num=(int)(67.89f);
+        System.out.println(num);
+
+        // int a=257;
+        // byte b=(byte)(a);
+        // System.out.println(b);
+
+        byte a=40;
+        byte c=20;
+        byte b=100;
+        int d=(a*b)/c;
+        System.out.println(d);
+    }
+}
