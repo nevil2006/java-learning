2ab0c2e06e9f1c36e52443a84164f6d7a2d39f1c
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-02-03 10:22:34 +0530

    first commit

---
+++ kunal kushwa java/practice questions/prints/two.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class two {
+    public static void main(String[]args){
+            Scanner sc=new Scanner(System.in);
+            char n = sc.next().charAt(0);
+            char a= sc.next().charAt(0);
+            char b = sc.next().charAt(0);
+            char d = sc.next().charAt(0);
+            System.out.print(n);
+            System.out.println(a);
+            System.out.println(b);  
+            System.out.println(d);
+    }
+
+}
