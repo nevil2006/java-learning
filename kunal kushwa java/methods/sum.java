1d7d964c11acf6d5e2a42b16076cfb71957eb9dc
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-03-02 04:23:47 +0530

    first commit

---
+++ kunal kushwa java/methods/sum.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class sum {
+    public static void main(String[] args) {
+        sum();
+    }
+    static void sum(){
+            Scanner sc=new Scanner(System.in);
+            System.out.println("Enter the value 1:");
+            int num1= sc.nextInt();
+            System.out.println("Enter the value 2:");
+            int  num2=sc.nextInt();
+            int  sum=num1+num2;
+            System.out.println("The sum =" +sum);
+
+    }
+}
+    
