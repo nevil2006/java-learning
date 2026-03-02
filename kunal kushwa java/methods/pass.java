cb718cc6a97a2e32a855144bf23092ecb369cac8
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-03-02 05:49:55 +0530

    first commit

---
+++ kunal kushwa java/methods/pass.java
@@ -0,0 +1,10 @@
+
+public class pass {
+    public static void main(String[] args) {
+        String name="Kunal kushwaha";
+        greet(name);
+    }   
+    static void greet(String name){
+        System.out.println(name);
+    }
+}
