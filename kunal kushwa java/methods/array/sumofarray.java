505f2db187a6aca0f85dabd9915499591b78fc34
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-03-08 07:59:19 +0530

    first commit

--- kunal kushwa java/methods/array/sumofarray.java
+++ kunal kushwa java/methods/array/sumofarray.java
@@ -4,11 +4,11 @@ public class sumofarray{
         int n=1234;
         int sum=0;
 
-        while(n<0){
+        while(n>0){
             int digit=n%10;
             sum=sum+digit;
             n=n/10;
         }
-    System.out.println("sum of digits"+ sum);
+    System.out.println("sum of digits: " + sum);
     }
 }
