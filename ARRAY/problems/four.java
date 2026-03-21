a914a45abf659982f8e741c4e2637c11ada1ba47
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-03-21 16:30:57 +0530

    first commit

---
+++ ARRAY/problems/four.java
@@ -0,0 +1,12 @@
+
+public class four {
+        public static void main(String[] args) {
+        int[]arr={10,20,3,4,5};
+        
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            sum+=arr[i];
+        }
+    System.out.println("sum: "+sum);
+    }
+}
