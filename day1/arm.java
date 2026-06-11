75de90990509f39cb2d23f7f9abfb9b65aaefe75
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-06-11 05:44:06 +0530

    hi

---
+++ day1/arm.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class arm{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n=sc.nextInt();
+
+        int temp=n;
+        int sum=0;
+
+        while(n>0){
+            int digit = n % 10;
+            sum = sum + (digit * digit * digit);
+            n = n /10;
+        }
+        if(sum == temp) {
+            System.out.println("Armstrong Number");
+        } else {
+            System.out.println("Not Armstrong Number");
+        }
+
+
+        }
+}
