9d7e1dfab65a37a4bd3cd9cf3b1d0336bb748b83
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-07-26 09:51:42 +0530

    a

--- day 1/Main.java
+++ day 1/Main.java
@@ -1,7 +1,7 @@
-public class Pre {
+public class Main {
     public static void main(String[] args) {
-        int a =10;
-        System.out.println(++a);
+        int a = 5;
+        System.out.println(++a + a++);
         System.out.println(a);
     }
 }
