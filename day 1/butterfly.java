0c990bec9870589be9929eb76c66988df6eb2689
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-01 16:27:22 +0530

    ad

--- day 1/butterfly.java
+++ day 1/butterfly.java
@@ -1,29 +1,29 @@
-public class diamond {
-
+public class butterfly{
     public static void main(String[] args) {
-
         int n = 4;
-
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= n - i; j++) {
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j = 1;j<=2*(n-i);j++){
                 System.out.print(" ");
             }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for (int i = n - 1; i >= 1; i--) {
 
-            for (int j = 1; j <= 2 * i - 1; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
-            System.out.println();
-        }
-
-        for (int i = n - 1; i >= 1; i--) {
-
-            for (int j = 1; j <= n - i; j++) {
+            for (int j = 1; j <= 2 * (n - i); j++) {
                 System.out.print(" ");
             }
 
-            for (int j = 1; j <= 2 * i - 1; j++) {
+            for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
 
