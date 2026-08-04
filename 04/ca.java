673bee6af96a95ab1e444c7ddc2a1049a4c59a7c
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-04 15:53:26 +0530

    a

---
+++ 04/ca.java
@@ -0,0 +1,17 @@
+public class ca {
+    public static void main(String[] args) {
+        int n = 28;
+        int sum = 0;
+        for(int i = 1; i <= n/2;i++){
+            if(n % i ==0){
+                sum +=i;
+            }
+        }
+        if(sum==n){
+            System.out.println("perfect number");
+        }else{
+            System.out.println("not perfect number");
+        }
+    }
+    
+}
