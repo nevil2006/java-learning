386d4f5d3a76b6e02cdab892964181f901a291b1
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-04 15:17:51 +0530

    A

---
+++ 04/lar.java
@@ -0,0 +1,15 @@
+import java.util.*;;
+public class lar {
+        public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int largest = Math.max(a, Math.max(b, c));
+
+        System.out.println("largest="+largest);
+    }
+}
