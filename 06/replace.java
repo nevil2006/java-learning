720ebdabce1545feaca73d83a67011e50a2c5b8b
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-06 17:48:58 +0530

    A

---
+++ 06/replace.java
@@ -0,0 +1,7 @@
+public class replace{
+        public static void main(String[] args) {
+        String str = "Hello world java";
+        String result = str.replace(" ","");
+        System.out.println(result);
+        }
+}
