785d7b6e3af850920c4ae66a54fee7c40bc6cd6c
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-12 20:20:28 +0530

    a

---
+++ 12/even.java
@@ -0,0 +1,17 @@
+public class even {
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 4};
+
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+
+            if (arr[i] % 2 == 0) {
+                sum += arr[i];
+            }
+        }
+
+        System.out.println(sum);
+    }
+}
