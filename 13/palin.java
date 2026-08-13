f20776d44714b19fd3aa09b4bf10642849690c35
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-13 19:05:37 +0530

    a

---
+++ 13/palin.java
@@ -0,0 +1,18 @@
+public class palin{
+        public static void main(String[] args) {
+            int n = 121;
+            int original = n;
+            int rev = 0;
+
+            while(n>0){
+                int digit = n % 10;
+                rev = rev * 10 + digit;
+                n = n /10;
+            }
+        if (original == rev) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+        }
+}
