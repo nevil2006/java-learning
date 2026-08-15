fe7d737a26e1548c590b3abcdb1c1f4fc81b4153
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-15 10:25:59 +0530

    A

--- 15/one.java
+++ 15/one.java
@@ -1,10 +1,15 @@
-public class one{
-        public static void main(String[] args) {
-            int n = 5;
-            for(int i = 1;i<n;i++){
-                for(int j = 1;j<=i;j++){
-                    System.out.println(j);
-                }
+public class one {
+    public static void main(String[] args) {
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
             }
+
+            System.out.println();
         }
+    }
 }
