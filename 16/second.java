35df3222bc5e17d45d57cf2fadd9568fc0d12ed5
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-16 08:00:57 +0530

    a

---
+++ 16/second.java
@@ -0,0 +1,25 @@
+public class second{
+        public static void main(String[] args) {
+
+        int[] arr = {10, 40, 20, 50, 30};
+
+        int largest = arr[0];
+        int secondLargest = Integer.MIN_VALUE;
+
+        for (int i = 1; i < arr.length; i++) {
+
+            if (arr[i] > largest) {
+                secondLargest = largest;
+                largest = arr[i];
+            }
+            else if (arr[i] > secondLargest && arr[i] < largest) {
+                secondLargest = arr[i];
+            }
+        }
+
+        System.out.println("Largest = " + largest);
+        System.out.println("Second Largest = " + secondLargest);
+
+
+    }
+}
