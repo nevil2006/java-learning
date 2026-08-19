b47262571d7f64379ff2fcd86da0cdd608ea15b8
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-19 19:55:30 +0530

    a

--- 19/zeroes.java
+++ 19/zeroes.java
@@ -1,3 +1,19 @@
-public class zeroes {
+public class zeroes{
+    public static void main(String[] args) {
+        int[]arr={0,1,0,2,3,0};
+        int i =0;
+        for(int j =0;j<arr.length;j++){
+            if(arr[j]!=0){
+                int temp =arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+                i++;
+
+            }
+        }
+        for(int num:arr){
+            System.out.print(num + " ");
+        }
+    }
     
 }
