00d59fe705adf8a3bcb2d9f3a43f17eecc868237
NEVIL <147979766+nevil2006@users.noreply.github.com>
2026-08-19 20:25:55 +0530

    a

--- 19/palin.java
+++ 19/palin.java
@@ -1,3 +1,28 @@
 public class palin {
-    
+    public static void main(String[] args) {
+
+        int[] arr = {1, 2, 3, 2, 1};
+
+        int left = 0;
+        int right = arr.length - 1;
+
+        boolean palindrome = true;
+
+        while (left < right) {
+
+            if (arr[left] != arr[right]) {
+                palindrome = false;
+                break;
+            }
+
+            left++;
+            right--;
+        }
+
+        if (palindrome) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
 }
